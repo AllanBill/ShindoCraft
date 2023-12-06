51e30bb683c2d303ebba1d04f223cc4a83c67d15
AllanBill <bill.allan.schuster@gmail.com>
2023-12-06 20:45:34 -0300

    Adição das espdas Sazuke (Kusanagui)

--- src/main/java/net/shindo/shindocraft/Block/ModBlocks.java
+++ src/main/java/net/shindo/shindocraft/Block/ModBlocks.java
@@ -17,41 +17,64 @@ import net.minecraftforge.registries.ForgeRegistries;
 import java.util.function.Supplier;
 
 public class ModBlocks {
-    public static final DeferredRegister<Block> BLOCKS
-           = DeferredRegister.create(ForgeRegistries.BLOCKS, ShindoCraft.MOD_ID);
+        public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
+                        ShindoCraft.MOD_ID);
 
-    public static final RegistryObject<Block> PERGAMINHOS_FOLHA = registerBlock("pergaminhos_folha",
-            () -> new Block(Block.Properties.of(Material.WOOD).noCollission().strength(4f, 1200f).harvestLevel(2).harvestTool(ToolType.AXE).lightLevel((state) -> 1)));
+        public static final RegistryObject<Block> PERGAMINHOS_FOLHA = registerBlock("pergaminhos_folha",
+                        () -> new Block(Block.Properties.of(Material.WOOD).noCollission().strength(4f, 1200f)
+                                        .harvestLevel(2).harvestTool(ToolType.AXE).lightLevel((state) -> 1)));
 
-    public static final RegistryObject<Block> PERGAMINHOS_AREIA = registerBlock("pergaminhos_areia",
-            () -> new Block(Block.Properties.of(Material.WOOD).noCollission().strength(4f, 1200f).harvestLevel(2).harvestTool(ToolType.AXE).lightLevel((state) -> 1)));
+        public static final RegistryObject<Block> PERGAMINHOS_AREIA = registerBlock("pergaminhos_areia",
+                        () -> new Block(Block.Properties.of(Material.WOOD).noCollission().strength(4f, 1200f)
+                                        .harvestLevel(2).harvestTool(ToolType.AXE).lightLevel((state) -> 1)));
 
-    public static final RegistryObject<Block> PERGAMINHOS_LUA = registerBlock("pergaminhos_lua",
-            () -> new Block(Block.Properties.of(Material.WOOD).noCollission().strength(4f, 1200f).harvestLevel(2).harvestTool(ToolType.AXE).lightLevel((state) -> 1)));
+        public static final RegistryObject<Block> PERGAMINHOS_LUA = registerBlock("pergaminhos_lua",
+                        () -> new Block(Block.Properties.of(Material.WOOD).noCollission().strength(4f, 1200f)
+                                        .harvestLevel(2).harvestTool(ToolType.AXE).lightLevel((state) -> 1)));
 
-    public static final RegistryObject<Block> PERGAMINHOS_CHUVA = registerBlock("pergaminhos_chuva",
-            () -> new Block(Block.Properties.of(Material.WOOD).noCollission().strength(4f, 1200f).harvestLevel(2).harvestTool(ToolType.AXE).lightLevel((state) -> 1)));
+        public static final RegistryObject<Block> PERGAMINHOS_CHUVA = registerBlock("pergaminhos_chuva",
+                        () -> new Block(Block.Properties.of(Material.WOOD).noCollission().strength(4f, 1200f)
+                                        .harvestLevel(2).harvestTool(ToolType.AXE).lightLevel((state) -> 1)));
 
-    public static final RegistryObject<Block> PERGAMINHOS_ESTRELA = registerBlock("pergaminhos_estrela",
-            () -> new Block(Block.Properties.of(Material.WOOD).noCollission().strength(4f, 1200f).harvestLevel(2).harvestTool(ToolType.AXE).lightLevel((state) -> 1)));
+        public static final RegistryObject<Block> PERGAMINHOS_ESTRELA = registerBlock("pergaminhos_estrela",
+                        () -> new Block(Block.Properties.of(Material.WOOD).noCollission().strength(4f, 1200f)
+                                        .harvestLevel(2).harvestTool(ToolType.AXE).lightLevel((state) -> 1)));
+
+        public static final RegistryObject<Block> PERGAMINHOS_NUVEM = registerBlock("pergaminhos_nuvem",
+                        () -> new Block(Block.Properties.of(Material.WOOD).noCollission().strength(4f, 1200f)
+                                        .harvestLevel(2).harvestTool(ToolType.AXE).lightLevel((state) -> 1)));
+
+        public static final RegistryObject<Block> PERGAMINHOS_NEVE = registerBlock("pergaminhos_neve",
+                () -> new Block(Block.Properties.of(Material.WOOD).noCollission().strength(4f, 1200f)
+                        .harvestLevel(2).harvestTool(ToolType.AXE).lightLevel((state) -> 1)));
+
+        public static final RegistryObject<Block> PERGAMINHOS_NEVOA = registerBlock("pergaminhos_nevoa",
+                () -> new Block(Block.Properties.of(Material.WOOD).noCollission().strength(4f, 1200f)
+                        .harvestLevel(2).harvestTool(ToolType.AXE).lightLevel((state) -> 1)));
+
+        public static final RegistryObject<Block> PERGAMINHOS_SOM = registerBlock("pergaminhos_som",
+            () -> new Block(Block.Properties.of(Material.WOOD).noCollission().strength(4f, 1200f)
+                    .harvestLevel(2).harvestTool(ToolType.AXE).lightLevel((state) -> 1)));
+
+        public static final RegistryObject<Block> PERGAMINHOS_PEDRA = registerBlock("pergaminhos_pedra",
+            () -> new Block(Block.Properties.of(Material.WOOD).noCollission().strength(4f, 1200f)
+                    .harvestLevel(2).harvestTool(ToolType.AXE).lightLevel((state) -> 1)));
 
 
 
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+                RegistryObject<T> toReturn = BLOCKS.register(name, block);
+                registerBlockItems(name, toReturn);
 
+                return toReturn;
+        }
 
-    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
-        RegistryObject<T> toReturn = BLOCKS.register(name, block);
-        registerBlockItems(name, toReturn);
+        private static <T extends Block> void registerBlockItems(String name, RegistryObject<T> block) {
+                ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                                new Item.Properties().tab(ModItemGroup.SHINDOCRAFT_GROUP)));
+        }
 
-        return toReturn;
-    }
-
-    private static <T extends Block> void registerBlockItems(String name,RegistryObject<T> block) {
-        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
-                new Item.Properties().tab(ModItemGroup.SHINDOCRAFT_GROUP)));
-    }
-
-    public static void register(IEventBus eventBus) {
-        BLOCKS.register(eventBus);
-    }
+        public static void register(IEventBus eventBus) {
+                BLOCKS.register(eventBus);
+        }
 }
--- src/main/java/net/shindo/shindocraft/item/ModItems.java
+++ src/main/java/net/shindo/shindocraft/item/ModItems.java
@@ -15,7 +15,11 @@ public class ModItems {
     public static final RegistryObject<Item> EMBLEMA_VILA_DA_AREIA = ITEMS.register("emblema_vila_da_areia",
             ()-> new Item(new Item.Properties().tab(ModItemGroup.SHINDOCRAFT_GROUP)));
 
+    public static final RegistryObject<Item> KUSANAGI_NO_TSURUGI_EM = ITEMS.register("kusanagi_no_tsurugi_em",
+            ()-> new Item(new Item.Properties().tab(ModItemGroup.SHINDOCRAFT_GROUP)));
 
+    public static final RegistryObject<Item> KUSANAGI_NO_TSURUGI_DES = ITEMS.register("kusanagi_no_tsurugi_des",
+            ()-> new Item(new Item.Properties().tab(ModItemGroup.SHINDOCRAFT_GROUP)));
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
